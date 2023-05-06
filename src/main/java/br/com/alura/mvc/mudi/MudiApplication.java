0c9dda8c63c2cb5a156d4e3f58ad67f0dcb2f701
Everton <imevertonrch@gmail.com>
2023-05-06 00:03:42 -0300

    Redirecionamento do context path para home

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -2,17 +2,21 @@ package br.com.alura.mvc.mudi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.sql.DataSource;
-
+@Controller
 @SpringBootApplication()
 public class MudiApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(MudiApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public ModelAndView redirectToServices() {
+        return new ModelAndView("redirect:/home");
     }
 }
