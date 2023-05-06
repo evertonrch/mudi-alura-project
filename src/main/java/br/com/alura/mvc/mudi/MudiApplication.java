e5050f81eb0b6faa1c41254966a330d11c33db7f
Everton <imevertonrch@gmail.com>
2023-05-06 01:05:49 -0300

    Removendo mapeamento de login

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -3,20 +3,11 @@ package br.com.alura.mvc.mudi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 @SpringBootApplication()
 public class MudiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MudiApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public ModelAndView redirectToServices() {
-        return new ModelAndView("redirect:/home");
     }
 }
